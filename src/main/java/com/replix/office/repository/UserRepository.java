cf04dfd002e1ba8175def5db043b62b71dfd65db
Your Name <yasitha.dev@gmail.com>
2025-01-19 18:04:28 +0530

    get permission list for user by join query.

--- src/main/java/com/replix/office/repository/UserRepository.java
+++ src/main/java/com/replix/office/repository/UserRepository.java
@@ -1,16 +1,42 @@
 package com.replix.office.repository;
 
+import com.replix.office.models.Permission;
 import com.replix.office.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
-    /*
-    @Query("SELECT p FROM User u JOIN u.roles r JOIN r.permissions p WHERE u.id = :userId")
-    Set<Permission> findPermissionsByUserId(@Param("userId") Long userId);
-    
-     */
+
+    @Query("    SELECT p\n" +
+            "    FROM Permission p\n" +
+            "    JOIN RolePermission rp ON p.id = rp.permission.id\n" +
+            "    JOIN Role r ON rp.role.id = r.id\n" +
+            "    JOIN UserRole ur ON r.id = ur.role.id\n" +
+            "    JOIN User u ON ur.user.id = u.id\n" +
+            "    WHERE u.id = :userId")
+    List<Permission> findPermissionsByUserId(@Param("userId") Integer userId);
+/*
+    SELECT p
+    FROM Permission p
+    JOIN RolePermission rp ON p.id = rp.permission.id
+    JOIN Role r ON rp.role.id = r.id
+    JOIN UserRole ur ON r.id = ur.role.id
+    JOIN User u ON ur.user.id = u.id
+    WHERE u.id = :userId
+
+    SELECT p.id, p.permission_name, p.description
+    FROM user u
+    JOIN user_role ur ON u.id = ur.user_id
+    JOIN role r ON ur.role_id = r.id
+    JOIN role_permission rp ON r.id = rp.role_id
+    JOIN permission p ON rp.permission_id = p.id
+    WHERE u.id = ?
+*/
+
 }
--- src/main/java/com/replix/office/security/UserDetailsServiceImpl.java
+++ src/main/java/com/replix/office/security/UserDetailsServiceImpl.java
@@ -1,5 +1,6 @@
 package com.replix.office.security;
 
+import com.replix.office.models.Permission;
 import com.replix.office.models.User;
 import com.replix.office.repository.UserRepository;
 import com.replix.office.security.jwt.JwtUtils;
@@ -35,8 +36,11 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user= userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
         System.out.println("/////////////////////users : " + user.getUsername() + " ////////////");//TODO:add proper logs
-        System.out.println("/////////////////////users roles name : " + user.getUserRoles().get(0).getRole().getRolePermissions().size() + "//////////////");
-        System.out.println("/////////////////////users permission sample name : " + user.getUserRoles().get(0).getRole().getRolePermissions().get(0).getPermission().getPermissionName() + "//////////////");
+        System.out.println("/////////////////////users roles name : " + user.getUserRoles().get(0).getRole().getRolePermissions().size() + "//////////////");//TODO:write unit testing
+        System.out.println("/////////////////////users permission sample name : " + user.getUserRoles().get(0).getRole().getRolePermissions().get(0).getPermission().getPermissionName() + "//////////////");//TODO:write unit testing
+        List<Permission> permissions= userRepository.findPermissionsByUserId(1);
+        System.out.println("/////////////////////permissions List Size : " + permissions.size() + "//////////////");//TODO:write unit testing
+        System.out.println("/////////////////////users permission sample name : " + permissions.get(0).getPermissionName() + "//////////////");//TODO:write unit testing
         return UserDetailsImpl.build(user);
     }
 
